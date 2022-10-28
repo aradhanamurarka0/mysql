826aebf6bc3cf370f4923dbf4918a6fd44167532
aradhanamurarka0 <115718448+aradhanamurarka0@users.noreply.github.com>
2022-10-28 20:02:16 +0530

    Add files via upload

--- costomers.sql
+++ costomers.sql
@@ -1,17 +1,8 @@
--- show databases;
-use costomers;
-create table Costomers_info(name varchar(255), id int, state varchar(255));
-insert into Costomers_info 
-values("Aradhana", 5, "Jaipur");
-select * from Costomers_info;
--- altering the table (1)
-alter table Costomers_info 
-add order_no varchar(255);
--- --updating a table(2)
-update Costomers_info 
-set  order_no="1015"
-where id=5;
--- deleating a row(3)
-delete from Costomers_info 
-where id= 2;
-
+show databases;
+create database instagram;
+use instagram;
+create table messages(msg_id int ,from_ varchar(20),to_ varchar(20) );
+create table posts(post_id int ,user_ varchar(20),date_ Date );
+create table User_(User_id int ,user_ varchar(20),date_ Date );
+create table story_(story_id int ,user_ varchar(20),date_ Date );
+show tables;
